8bcdef1d57f617d4ac3fee2baf8bd6898bfa6fed
Nick Gonzales <nickwgonzales@gmail.com>
2020-07-10 16:36:45 -0500

    added new collection components that go on the new shop page, no link to shop page yet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import { HomePage } from './pages/homepage/homepage.component.jsx';
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
+ 
 import { Route, Switch } from 'react-router-dom';
-
-const tester = () => (
-  <div>
-    <h1>hats!!!</h1>
-  </div>
-)
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop/hats' component={tester} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import SHOP_DATA from './collections.data.js';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+
+class ShopPage extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
+
+    render(){
+        const {collections} = this.state;
+        return (
+            <div className='shop-page'>
+                {
+                    collections.map( 
+                        collection => (<CollectionPreview 
+                            title={collection.title}
+                            items={collection.items}
+                            key={collection.id}  />
+                    ))
+                }
+            </div>
+            
+        )
+    }
+}
+
+export default ShopPage;
