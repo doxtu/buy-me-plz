22ddaa453974f5c8f9eb07a05b19002b566e74ea
Nick Gonzales <nickwgonzales@gmail.com>
2020-07-16 13:44:58 -0500

    created cart item component and implemented creating the elements from the cartItems state in the cart-dropdown component

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './cart-item.styles.scss';
+
+const CartItem = ({item: {name, imageUrl, price, quantity}}) => (
+    <div className='cart-item'>
+        <img alt='item' src={imageUrl} />
+        <div className='item-details'>
+            <span className='name'>{name}</span>
+            <span className='price'>{`$${price} x ${quantity}`} </span>
+        </div>
+    </div>
+);
+
+export default CartItem;
